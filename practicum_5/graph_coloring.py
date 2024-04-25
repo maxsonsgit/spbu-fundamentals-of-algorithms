7c75b9c8bcbfd7117747a329ea85a01373793a7f
maksson <son.mp146@gmail.com>
2024-04-25 19:43:00 +0300

    classwork5

--- practicum_5/graph_coloring.py
+++ practicum_5/graph_coloring.py
@@ -33,31 +33,45 @@ def set_colors(G, colors):
 
 
 def tweak(colors, n_max_colors):
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
-
-    pass
+    new_colors = colors.copy()
+    new_colors[np.random.randint(0, len(colors) - 1)] = np.random.randint(0, n_max_colors - 1)
+    return new_colors
 
 
 def solve_via_hill_climbing(
     G: nx.Graph, n_max_colors: int, initial_colors: NDArrayInt, n_iters: int
 ) -> NDArrayInt:
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    loss_history = np.zeros((n_iters,), dtype=np.int_)
+    n_tweaks = 10
+    cur_colors = initial_colors
+    next_colors = initial_colors.copy()
+    next_colors_best = initial_colors.copy()
 
-    pass
+    for i in range(n_iters):
+        loss_history[i] = number_of_conflicts(G, cur_colors)
+        next_colors_best = tweak(cur_colors, n_max_colors)
+        n_conflicts_best = number_of_conflicts(G, next_colors_best)
+        for _ in range(n_tweaks):
+            next_colors = tweak(cur_colors, n_max_colors)
+            if number_of_conflicts(G, next_colors) < n_conflicts_best:
+                next_colors_best = next_colors
+                n_conflicts_best = number_of_conflicts(G, next_colors)
+        if n_conflicts_best < number_of_conflicts(G, cur_colors):
+            cur_colors = next_colors_best
+
+    return loss_history
 
 
 def solve_via_random_search(
     G: nx.Graph, n_max_colors: int, initial_colors: NDArrayInt, n_iters: int
 ) -> NDArrayInt:
-    ##########################
-    ### PUT YOUR CODE HERE ###
-    ##########################
+    loss_history = np.zeros((n_iters,), dtype=np.int_)
 
-    pass
+    for _ in range(n_max_iters):
+        colors = np.random.randint(low=0, high=n_max_colors-1, size=len(G.nodes))
+        loss_history[_] = number_of_conflicts(G, colors)
+
+    return loss_history
 
 
 def solve_with_restarts(
@@ -114,4 +128,5 @@ if __name__ == "__main__":
         n_max_iters,
         n_restarts,
     )
+
     plot_loss_history(loss_history)
