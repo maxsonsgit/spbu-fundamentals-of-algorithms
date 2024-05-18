1d8091ecb7dcbabdcd8223505ec1ac02ecacf42f
maksson <son.mp146@gmail.com>
2024-05-18 17:33:28 +0300

    small fixes

--- practicum_2/homework/advanced/cycles_in_directed_graph.py
+++ practicum_2/homework/advanced/cycles_in_directed_graph.py
@@ -9,7 +9,7 @@ def iteration(G: nx.DiGraph, vertex, visited: list[bool], flag: list[bool], star
         for neighbor in G.neighbors(vertex):
             if not (neighbor == start[0]):
                 if not visited[int(neighbor)]:
-                    iter(G, neighbor, visited, flag, start)
+                    iteration(G, neighbor, visited, flag, start)
             else:
                 flag[0] = True
                 break
