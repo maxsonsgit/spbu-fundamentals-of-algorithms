de384cc96e91797f10e50ebcf9852f06d463b4e1
maksson <son.mp146@gmail.com>
2024-05-18 17:31:07 +0300

    add itteration func to cycles_in...

--- practicum_2/homework/advanced/cycles_in_directed_graph.py
+++ practicum_2/homework/advanced/cycles_in_directed_graph.py
@@ -1,5 +1,19 @@
 import networkx as nx
-from iteration import iteration
+# from iteration import iteration
+
+
+def iteration(G: nx.DiGraph, vertex, visited: list[bool], flag: list[bool], start: list[str]):
+    visited[int(vertex)] = True
+
+    if not flag[0]:
+        for neighbor in G.neighbors(vertex):
+            if not (neighbor == start[0]):
+                if not visited[int(neighbor)]:
+                    iter(G, neighbor, visited, flag, start)
+            else:
+                flag[0] = True
+                break
+    return flag
 
 
 def has_cycles(G: nx.DiGraph):
